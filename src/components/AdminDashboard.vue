5e62eca3a2e0679e4f7ec0f381a8bfe2a39efa72
Meg Finnegan <mfinnegan101@gmail.com>
2021-11-30 10:17:57 -0500

    add pending listings to dashboard

--- src/components/AdminDashboard.vue
+++ src/components/AdminDashboard.vue
@@ -3,48 +3,15 @@
     <div class="min-h-screen"> 
       <div class="grid grid-cols-5 grid-rows-2 gap-3 p-5">
         <div class="col-start-1 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">15 Total Listings</div> 
-        <div class="col-start-2 col-span-4 row-start-1 row-end-3 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 pb-20 rounded-lg">Positions Density</div>
+        <div class="col-start-2 col-span-4 row-start-1 row-end-3 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 pb-20 rounded-lg">
+          <p>Positions Density</p>
+          
+
+        </div>
         <div class="relative col-start-2 col-span-4 row-start-3 row-end-6 bg-gray-100 text-gray-500 text-lg font-bold text-center py-0 px-10 rounded-lg">
             
-            <div class="min-h-screen flex items-center">
-              <div class="flex overflow-x-scroll">
-                <div class="flex flex-nowrap">
-                  <div class="inline-block px-3">
-                    <div class="bg-gray-400 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Pending Listings</div>
-                  </div>
-                  <div class="inline-block px-3">
-                    <div class="bg-gray-400 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Pending Listings</div>
-                  </div>
-                  <div class="inline-block px-3">
-                    <div class="bg-gray-400 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Pending Listings</div>
-                  </div>
-                  <div class="inline-block px-3">
-                    <div class="bg-gray-400 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Pending Listings</div>
-                  </div>
-                  <div class="inline-block px-3">
-                    <div class="bg-gray-400 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Pending Listings</div>
-                  </div>
-                  <div class="inline-block px-3">
-                   <div class="bg-gray-400 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Pending Listings</div>
-                  </div>
-                  <div class="inline-block px-3">
-                    <div class="bg-gray-400 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Pending Listings</div>
-                  </div>
-                  <div class="inline-block px-3">
-                    <div class="bg-gray-400 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Pending Listings</div>
-                  </div>
-                  <div class="inline-block px-3">
-                    <div class="bg-gray-400 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Pending Listings</div>
-                  </div>
-                  <div class="inline-block px-3">                  
-                    <div class="bg-gray-400 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Pending Listings</div>
-                  </div>
-                  <div class="inline-block px-3">
-                    <div class="bg-gray-400 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Pending Listings</div>
-                  </div>
-                </div>
-              </div>
-            </div> 
+        <PendingListingModule/>
+
         </div>
         <div class="col-start-1 row-start-2 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Most Viewed Company</div>
         <div class="col-start-1 row-start-3 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Software Engineer: Most Viewed Position</div>
@@ -56,14 +23,20 @@
   </section>
 </template>
 
+
+
 <script>
 import AdminStats from "./AdminStats.vue";
 import AdminCard from "./AdminCard.vue";
+import PendingListingModule from "./PendingListingModule.vue";
+
 export default {
   name: "AdminDashboard",
+
   components: {
     AdminStats,
     AdminCard,
-  },
+    PendingListingModule,
+  }
 };
 </script>
---
+++ src/components/data/PieChart.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="width:300 height:300 border-radius:50% border:2px background:conic-gradient()"></div>
+</template>
+
+<script>
+
+export default PieChart({
+    setup() {
+        
+    },
+})
+
+
+</script>
+
